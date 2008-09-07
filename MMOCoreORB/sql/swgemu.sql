74ec46bb456b8af7a14ee2fca0e8a63adcab2eef
McMahon <McMahon@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2008-09-07 16:15:43 +0000

    [added] Vehicle Deeds to Blue Frog [removed] Most of the default datapad vehicles - please use the deeds. [added] House/Installation Code - Not complete/Live yet [sql update required]
    git-svn-id: http://svn2.assembla.com/svn/swgemu/core3/trunk@674 c3d1530f-68f5-4bd0-87dc-8ef779617e40

--- MMOCoreORB/sql/swgemu.sql
+++ MMOCoreORB/sql/swgemu.sql
@@ -83098,3 +83098,22 @@ INSERT INTO `warp` VALUES ('55', 'mos_entha_shuttle_b', '8', '1395.000000', '0.0
 INSERT INTO `warp` VALUES ('56', 'yavin_mining_op', '9', '-268.000000', '35.000000', '4897.000000', 'Mining Outpost, Yavin IV', '3', '180');
 INSERT INTO `warp` VALUES ('57', 'yavin_imperial_op', '9', '4051.000000', '37.000000', '-6215.000000', 'Imperial Outpost, Yavin IV', '3', '180');
 INSERT INTO `warp` VALUES ('58', 'yavin_labor_op', '9', '-6921.000000', '73.000000', '-5726.000000', 'Labor Outpost, Yavin IV', '3', '180');
+
+CREATE TABLE IF NOT EXISTS `character_structures` (
+  `zoneid` tinyint(4) NOT NULL,
+  `objectid` bigint(20) NOT NULL,
+  `parentid` bigint(20) NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `template` varchar(255) NOT NULL,
+  `crc` bigint(20) NOT NULL,
+  `file` varchar(200) NOT NULL,
+  `oX` float NOT NULL,
+  `oY` float NOT NULL,
+  `oZ` float NOT NULL,
+  `oW` float NOT NULL,
+  `X` float NOT NULL,
+  `Z` float NOT NULL,
+  `Y` float NOT NULL,
+  `type` float NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
